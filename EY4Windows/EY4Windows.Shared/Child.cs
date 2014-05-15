d1549b5c6ba64fcf321e73b751d1e9e6526eb8bc
mwares <markrwares@gmail.com>
2014-05-15 14:22:01 +0100

    Initial EY4Windows check in

---
+++ EY4Windows/EY4Windows.Shared/Child.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EY4Windows
+{
+    public class Child
+    {
+         private string name;
+
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+        private string type;
+
+        public string Type
+        {
+            get { return type; }
+            set { type = value; }
+        }
+
+        private string image;
+
+        public string Image
+        {
+            get { return image; }
+            set { image = value; }
+        }
+
+        public Child()
+        {
+        }
+    }
+}
